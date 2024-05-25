1bbc20cd7daecd76b5cafd12462d135462713071
shaohanzhong <=>
2024-05-25 09:57:29 +0800

    openai

---
+++ src/OpenAIClient/OpenAIService.cs
@@ -0,0 +1,28 @@
+﻿using Flurl.Http;
+using Flurl.Http.Configuration;
+
+namespace OpenAIClient;
+
+internal class OpenAIService
+{
+    private readonly IFlurlClient _client;
+
+    public OpenAIService(IFlurlClientCache clientCache)
+    {
+        _client = clientCache.Get("OpenAI");
+    }
+
+    public async Task ChatCompletionAsync()
+    {
+        var result = await _client.Request("/v1/chat/completions").PostJsonAsync(new
+        {
+            model = "gpt-3.5-turbo-16k",
+            messages = new[]
+            {
+                new{role="system",content="你是一位非常优秀的AI助手" },
+                new{role="user",content="中国有多少年的历史文化" },
+            }
+        });
+        var test = await result.GetStringAsync();
+    }
+}
--- src/OpenAIClient/Program.cs
+++ src/OpenAIClient/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Flurl.Http;
+using Flurl.Http.Configuration;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using OpenAIClient;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Configuration.AddUserSecrets<Program>();
+
+builder.Services.AddSingleton(sp =>
+{
+    return new FlurlClientCache()
+    .Add("OpenAI", "https://api.openai.com", clientBuilder =>
+    {
+        clientBuilder.WithOAuthBearerToken("sk-proj-xxx");
+    });
+});
+builder.Services.AddTransient<OpenAIService>();
+
+var host = builder.Build();
+var aiService = host.Services.GetRequiredService<OpenAIService>();
+await aiService.ChatCompletionAsync();
+
+await host.RunAsync();
